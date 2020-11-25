867521b898f88680bea547cec0a5ae7795779f2c
Igor Trifonov <Ha1lowe3n@yandex.ru>
2020-11-25 23:54:28 +0800

    feat(script.js): added modal 1

--- src/index.js
+++ src/index.js
@@ -16,13 +16,55 @@ document.addEventListener('DOMContentLoaded', () => {
 
     document.body.addEventListener('click', (e) => {
       if (e.target.closest('.clubs-list')) {
-        e.preventDefault();
         toggleClubsList();
       } else {
-        e.preventDefault();
         clubsList.style.display = 'none';
       }
     });
   };
   showClubsMenu();
+
+
+  // Modal windows
+
+  const modalWindows = () => {
+    //modal 1, id = free_visit_form
+    const modalOne = () => {
+      const modal = document.getElementById('free_visit_form'),
+            openModal = document.querySelector('.open-popup'),
+            modelForm = document.getElementById('form2');
+
+      const clearForm = () => {
+        [...modelForm.elements].forEach(item => {
+          if (item.tagName.toLowerCase() !== 'button' && item.type !== 'button') {
+            item.value = '';
+          }
+        });
+      };
+
+      openModal.addEventListener('click', () => {
+        modal.style.display = 'block';
+      });
+
+      modal.addEventListener('click', (e) => {
+        let target = e.target;
+
+        if (target.classList.contains('popup-close')) {
+          modal.style.display = 'none';
+          clearForm();
+        } else {
+          target = target.closest('.form-content');
+    
+          if (!target) {
+            modal.style.display = 'none';
+            clearForm();
+          }
+        }
+      });
+    };
+    modalOne();
+
+  };
+  modalWindows();
+
 });
