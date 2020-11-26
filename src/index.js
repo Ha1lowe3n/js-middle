23fc8dbcedea47bdab11d063dea40ca1ac88c907
Igor Trifonov <Ha1lowe3n@yandex.ru>
2020-11-27 00:53:21 +0800

    feat(slider.js): added main slider

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import showClubsMenu from './modules/showClubsMenu';
 import modalWindows from './modules/modalWindows';
 import forms from './modules/forms';
 import giftModal from './modules/giftModal';
+import slider from './modules/slider';
 
 
 showClubsMenu();
 modalWindows();
 forms();
 giftModal();
+slider();
 
